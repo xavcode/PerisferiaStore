d0374342a915c6d3065bb968af1608cc8741e5c9
Fausto Lalia <lmfaustolm@gmail.com>
2023-05-09 20:13:53 -0300

    models, controllers, routes, db, app, index

---
+++ backend/src/models/User.js
@@ -0,0 +1,31 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    // defino el modelo
+    sequelize.define('user', {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        unique: true,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+      },
+      mail: {
+        type: DataTypes.STRING,
+      },
+      img: {
+        type: DataTypes.STRING,
+      },
+      phone: {
+        type: DataTypes.STRING,
+      }
+    });
+  };
+  
