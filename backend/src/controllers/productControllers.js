7690f2ab3db5ce080fa17810f80508703da1eeac
Javier Diaz <javiermauriciodiaz@gmail.com>
2023-05-13 04:52:24 -0500

    routes and controllers for create products, get all products and search by name working,

--- backend/src/controllers/productControllers.js
+++ backend/src/controllers/productControllers.js
@@ -1,67 +1,63 @@
-const { Product } = require('../db.js');
+const { Products } = require('../db.js');
 
+//get all products from DB
+const getAllProducts = async () => {
+  try {
+    const allProducts = await Products.findAll()
+    const products = allProducts?.map((product) => product)
+    return (products)
+  } catch (error) {
+    throw new Error(error.message)
+  }
+}
 
-const getAllProducts = async (req, res) => {
-    try {
-    const products = await Product.findAll();
-    res.status(200).json(products);
-    } catch (error) {
-    res.status(500).json({ message: error.message });
-    } 
+//----------------------------------------------------------------//
+
+const getProductsByName = async (productSearch) => {
+  try {
+    const product = await Products.findAll({
+      where: { name: { [Op.like]: `%${product}%` } },
+    });
+    return (product);
+  } catch (error) {
+    throw new Error({ message: 'Server Error' });
+  }
 };
 
 
 //----------------------------------------------------------------//
 
-const getProductById = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const product = await Product.findByPk(id);
-        if (!product) {
-        return res.status(404).json({ message: 'Product not found' });
-        }
-        return res.status(200).json(product);
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ message: 'Server Error' });
-    }
-    };
-
+// const getProductById = async (req, res) => {
+//   try {
+//     const { id } = req.params;
+//     const product = await Products.findByPk(id);
+//     if (!product) {
+//       return res.status(500).json({ message: 'Product not found' });
+//     }
+//     return res.status(200).json(product);
+//   } catch (error) {
+//     console.error(error);
+//     return res.status(500).json({ message: 'Server Error' });
+//   }
+// };
 
 //----------------------------------------------------------------//
 
-    const getProductsByName = async (req, res) => {
-        try {
-        const { name } = req.params;
-        const products = await Product.findAll({
-            where: { name: { [Op.like]: `%${name}%` } },
-        });
-        return res.status(200).json(products);
-        } catch (error) {
-        console.error(error);
-        return res.status(500).json({ message: 'Server Error' });
-        }
-    };
-
-    
-//----------------------------------------------------------------//
-
-const createProduct = async (req, res) => {
-    try {
-        const { name, price, img, status, description, rating, category, brand } = req.body;
-        const newProduct = await Product.create({ name, price, img, status, description, rating, category, brand });
-        res.status(201).json(newProduct);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ message: 'Internal server error' });
-    }
-};
+// const createProduct = async (req, res) => {
+//   try {
+//     const { name, price, img, status, description, rating, category, brand } = req.body;
+//     const newProduct = await Product.create({ name, price, img, status, description, rating, category, brand });
+//     res.status(201).json(newProduct);
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).json({ message: 'Internal server error' });
+//   }
+// };
 
 //----------------------------------------------------------------//
 
 module.exports = {
-    getAllProducts,
-    getProductById,
-    getProductsByName,
-    createProduct
+  getAllProducts,
+  // getProductById,
+  getProductsByName,
 };
