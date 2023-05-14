d929004f532a731aff23314e08bf13888df62d7e
Leonel aguilera <Leonelcarp142nuevo@gmail.com>
2023-05-13 21:53:58 -0300

    Nuevo cambio

--- frontend/src/pages/Contact/Contact.jsx
+++ frontend/src/pages/Contact/Contact.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div className=' text-white mt-[300px] flex justify-center items-center font-bold'>
+    <div className=' text-white mt-[200px] flex justify-center items-center font-bold'>
 
       <section className='flex flex-col text-center my-3'>
 
-        <h1 className='text-3xl'>Contacto</h1> 
+        <h1 className='text-3xl'>Contacto</h1>
         <p>Por favor, completa el siguiente formulario para contactarnos.</p>
         <form className='flex flex-col bg-inherit gap-2'>
           <label htmlFor="name">Nombre:</label>
