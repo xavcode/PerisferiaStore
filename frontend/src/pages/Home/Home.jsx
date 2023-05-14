9a2579fc01b27bb14d363d94b8259ad05ed0313f
Leonel aguilera <Leonelcarp142nuevo@gmail.com>
2023-05-13 21:19:18 -0300

    Cambio leonel

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
-import homeImage from "../../assets/images/home-image.jpg";
+import { AiFillCustomerService } from "react-icons/ai";
+import { AiFillDollarCircle } from "react-icons/ai"; //AiOutlineDollar
+import { AiFillLock } from "react-icons/ai"; //AiOutlineUnlock
+import { AiFillStar } from "react-icons/ai"; //AiOutlineStar
+import { AiOutlineFundProjectionScreen } from "react-icons/ai";
+import "../Home/Home.css";
 
 const Home = () => {
   const images = [
@@ -16,7 +21,7 @@ const Home = () => {
   useEffect(() => {
     const timer = setInterval(() => {
       setCurrentImage((prevImage) => (prevImage + 1) % images.length);
-    }, 12000);
+    }, 3000);
 
     return () => clearInterval(timer);
   }, [images.length]);
@@ -32,26 +37,91 @@ const Home = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <div className="relative">
-        <p>Encuentra tu compañero perfecto en nuestra tienda en línea. Descubre una amplia selección de productos y accesorios que harán que cada día sea especial. Explora nuestras increíbles ofertas y encuentra todo lo que necesitas para tu estilo de vida. ¡Haz de las compras una experiencia emocionante y descubre el mundo de posibilidades que tenemos para ti en nuestra tienda en línea!</p>
-        <img
-          className="w-full max-w-2xl rounded-lg shadow-lg"
-          src={images[currentImage]}
-          alt="Slide"
-        />
-        <button
-          className="absolute left-0 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white px-4 py-2 rounded-full"
-          onClick={previousImage}
-        >
-          &#8249;
-        </button>
-        <button
-          className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white px-4 py-2 rounded-full"
-          onClick={nextImage}
-        >
-          &#8250;
-        </button>
+    <div className="text-left">
+      <main className="flex justify-between gap-20 max-w-2xl mx-auto">
+        <div className="max-w-350 mt-20 relative">
+          <div className="flex items-start">
+            <div>
+            <h1 className="font-nunito text-6xl font-extrabold leading-13 tracking-0.5% text-black mr-20">
+              Descubre una amplia selección de productos y accesorios.
+            </h1>
+            {/* <h5>aa</h5> */}
+            <p className="text-15 font-semibold text-gray-600 leading-5 max-w-285 mb-8">
+              Explora increíbles ofertas y encuentra todo lo que
+              necesitas para tu estilo de vida.<h1 className="text-red-200">
+                Haz de las compras una
+              experiencia emocionante y descubre un mundo de posibilidades en
+              nuestra tienda en línea.</h1> 
+              </p> 
+            </div>
+            <img
+              className="w-full h-[600px] w-[600px] h-auto rounded-lg shadow-lg object-cover "
+              src={images[currentImage]}
+              alt="Slide"
+              />
+          </div>
+          {/* <button
+            className="absolute left-20 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white px-4 py-2 rounded-full"
+            onClick={previousImage}
+            >
+            &#8249;
+            </button>
+            <button
+            className="absolute right-20 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white px-4 py-2 rounded-full"
+            onClick={nextImage}
+            >
+            &#8250;
+          </button> */}
+        </div>
+      </main>
+
+      {/*Cartas Inferor de la pagina*/}
+
+      <div className="flex justify-between max-w-5xl mx-auto mt-16 px-1">
+        <div className="flex items-center flex-col gap-4 h-46 w-46 p-1 rounded-lg shadow-lg transition duration-300 ease-in-out hover:shadow-xl bg-gray-500">
+          <h1 className="text-5xl text-gray-800" alt="icon">
+            <AiFillStar />
+          </h1> 
+          <p className="text-sm font-semibold text-gray-800">
+            Encuentra productos <br /> perfectos para ti.
+          </p>  
+        </div>
+
+        <div className="flex items-center flex-col gap-4 h-46 w-46 p-1 rounded-lg shadow-lg transition duration-300 ease-in-out hover:shadow-xl bg-gray-500">
+          <h1 className="text-5xl text-gray-800" alt="icon">
+            <AiFillCustomerService />
+          </h1>
+          <p className="text-sm font-semibold text-gray-800">
+            Transforma tus deseos en realidad.
+          </p>
+        </div>
+
+        <div className="flex items-center flex-col gap-4 h-46 w-46 p-1 rounded-lg shadow-lg transition duration-300 ease-in-out hover:shadow-xl bg-gray-500">
+          <h1 className="text-5xl text-gray-800" alt="icon">
+            <AiFillDollarCircle />
+          </h1>
+          <p className="text-sm font-semibold text-gray-800">
+            Descuentos exclusivos 30% al 50%.
+          </p>
+        </div>
+
+        <div className="flex items-center flex-col gap-4 h-46 w-46 p-1 rounded-lg shadow-lg transition duration-300 ease-in-out hover:shadow-xl bg-gray-500">
+          <h1 className="text-5xl text-gray-800" alt="icon">
+            <AiOutlineFundProjectionScreen />
+          </h1>
+          <p className="text-sm font-semibold text-gray-800">
+            Todo lo que buscas en un solo sitio.
+          </p>
+        </div>
+
+        <div className="flex items-center flex-col gap-4 h-46 w-46 p-1 rounded-lg shadow-lg transition duration-300 ease-in-out hover:shadow-xl bg-gray-500">
+          <h1 className="text-5xl text-gray-800" alt="icon">
+            <AiFillLock />
+          </h1>
+          <p className="text-sm font-semibold text-gray-800">
+            Tu <br /> compra Segura.
+          </p>
+        </div>
       </div>
     </div>
   );
