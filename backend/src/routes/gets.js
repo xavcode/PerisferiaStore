e1012402402f61db04c7a45b18d0c376830d5238
Cesar Gabriel Londero <gabriellondero40@.com>
2023-05-16 07:55:34 -0300

    nueva peticion para traer el valor con el id en backend/routes/gets.js

--- backend/src/routes/gets.js
+++ backend/src/routes/gets.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
 const { createRecordProduct } = require('../controllers/productControllers');
 const { getAllProducts } = require('../controllers/productControllers');
+const { addProductById } = require('../controllers/addProductById');
 const router_get = Router();
 
-router_get.get('/', createRecordProduct)
-router_get.get('/store', getAllProducts)
+router_get.get('/', createRecordProduct);
+router_get.get('/store', getAllProducts);
+router_get.get('/store/:id', addProductById);
 
 module.exports = router_get
