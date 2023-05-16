0001fc6ac4b1b093e119b07a1c2bb0d74138a84b
gabriellondero40@gmail.com <gabriellondero40@gmail.com>
2023-05-16 11:55:00 -0300

    base de datos actualizada con nuevos valores

--- backend/src/routes/gets.js
+++ backend/src/routes/gets.js
@@ -3,12 +3,17 @@ const { createRecordProduct } = require('../controllers/productControllers');
 const { getAllProducts } = require('../controllers/productControllers');
 const { addProductById } = require('../controllers/addProductById');
 const { get_Products_By_Name } = require('../controllers/addProductByName');
+const datab = require('../datab.json')
 const router_get = Router();
 
 router_get.get('/', createRecordProduct);
 router_get.get('/store', getAllProducts);
 router_get.get('/store/:id', addProductById);
 router_get.get('/name', get_Products_By_Name);
+router_get.get('/json', (req, res) => {
+    const jsonData = datab;
+    res.send(jsonData.products)
+})
 
 
 
