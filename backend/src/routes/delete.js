9d25982b931809e9380d1e97a39898d730e25ab9
Alan Almeida <alandavidalmeida@gmail.com>
2023-05-20 17:52:04 -0300

    borrado lógico

--- backend/src/routes/delete.js
+++ backend/src/routes/delete.js
@@ -1,7 +1,8 @@
 const { Router } = require('express');
 const { delete_favorite } = require('../controllers/deleteFavorites');
+const { delete_Product } = require('../controllers/deleteProducts');
 const router_delete = Router();
 
 router_delete.delete('/user/favorites', delete_favorite)
-
+router_delete.delete('/store/:id', delete_Product);
 module.exports = router_delete
