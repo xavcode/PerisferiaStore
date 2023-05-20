3738534916f6b39133128a70db5398bd7a36c43d
Alan Almeida <alandavidalmeida@gmail.com>
2023-05-20 15:41:36 -0300

    actualizando

--- frontend/src/components/Cards/Cards.jsx
+++ frontend/src/components/Cards/Cards.jsx
@@ -21,7 +21,7 @@ const Cards = () => {
   const productsToRender = products.filter((product) => {
     const priceInRange = product.price >= minPrice && product.price <= maxPrice;
     const matchesCategory = category === 'all' || category === product.category;
-    const matchesSearch = searched === '' || product.title.includes(searched);
+    const matchesSearch = searched === '' || product.name.includes(searched);
 
     return priceInRange && matchesCategory && matchesSearch;
   });
