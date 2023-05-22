997cfb173a4e10809ff8e69e6109bb78e2ffce0b
Javier Diaz <javiermauriciodiaz@gmail.com>
2023-05-22 00:30:13 -0500

    users rout in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,17 +15,13 @@ import ProductForm from './admin/components/CreateProduct/ProductForm';
 import EditProduct from './admin/pages/Products/EditProduct';
 import Products from './admin/pages/Products/Products';
 import Layout from './admin/components/Layout/Layout';
-import Users from './admin/pages/Users/Users';
-import { CreateUserForm } from './admin/components/CreateUser/CreateUserForm';
-import { User, useAuth0 } from '@auth0/auth0-react';
 
 function App() {
-  // const {user} = useAuth0();
+
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-
         <Route path='/' element={<Home />} />
         <Route exact path='/about' element={<About />} />
         <Route exact path='/store' element={<Store />} />
@@ -35,17 +31,11 @@ function App() {
         <Route exact path='/register' element={<Register />} />
 
         <Route  path= '/admin' >
-          <Route index element={<Layout/>}/>
-          
+          <Route/>
           <Route  path='products' element={<Products />} />
           <Route  path='/admin/products/create' element={<ProductForm />} />
           <Route  path='/admin/products/edit/:id' element={<EditProduct />} />
-          <Route  path='/admin/users' element={<Users/>} />
-
-          <Route  path='/admin/users/create' element={<CreateUserForm />} />
-          <Route  path='/admin/users/edit/:id' element={<CreateUserForm />} />
         </Route>
-       
         <Route path='*' element={<Error />} />
       </Routes>
     </BrowserRouter>
