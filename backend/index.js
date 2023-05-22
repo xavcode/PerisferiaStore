c6ffce63adacb080898059a6b02a9f6df35c5e70
Javier Diaz <javiermauriciodiaz@gmail.com>
2023-05-22 13:04:03 -0500

    url railways

--- backend/index.js
+++ backend/index.js
@@ -40,7 +40,7 @@ $$ |     $$$$$$$$\$$ |  $$ $$$$$$\\$$$$$$  $$ |     $$$$$$$$\$$ |  $$ $$$$$$\$$ 
 
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js'); 
-const {PORT} = process.env.PORT || 3000
+const {PORT} = process.env
 // Syncing all the models at once.
 conn.sync({ alter: true }).then(() => {
     console.log('DB Connected')
