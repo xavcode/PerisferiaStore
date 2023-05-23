157b3f46cacbf61cd02cca04da715ba9803cd625
gabriellondero40@gmail.com <gabriellondero40@gmail.com>
2023-05-23 18:36:11 -0300

    funcionalidad de editado para users

--- backend/src/routes/puts.js
+++ backend/src/routes/puts.js
@@ -3,8 +3,10 @@ const fs = require('fs');
 const router_put = Router();
 const multer = require('multer');
 const { initialEdit } = require('../controllers/editProduct');
+const { initialEdit_user } = require('../controllers/editUser');
 const upload = multer({ dest: 'uploads/' })
 
-router_put.put('/product', initialEdit)
+router_put.put('/product', initialEdit);
+router_put.put('/user', initialEdit_user);
 
 module.exports = router_put;
