0fec33e5f5d2dde481a018ffd07644e3d450dcd8
gabriellondero40@gmail.com <gabriellondero40@gmail.com>
2023-05-23 15:34:18 -0300

    incluyendo los ultimos cambios, dev actualizada

--- backend/index.js
+++ backend/index.js
@@ -42,11 +42,11 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js'); 
 const {PORT} = process.env
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ alter: true }).then(() => {
     console.log('DB Connected')
     // Agregamos CORS al servidor 
 
     server.listen(PORT, () => {
-        console.log('Listening on Port: 3001 happy Codding (❁´◡`❁)'); // eslint-disable-line no-console
+        console.log(`Listening on Port: ${PORT} happy Codding *^____^*`); // eslint-disable-line no-console
     });
 });
