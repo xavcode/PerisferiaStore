0718186cda46c26e28933c3e8fb466eae35c4164
ThomasNovella <thomasnovella12@gmail.com>
2023-05-24 10:27:35 -0300

    haciendo el log in, un PopUp

--- backend/src/db.js
+++ backend/src/db.js
@@ -7,7 +7,7 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT 
 } = process.env;
  
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:/${DB_NAME}`, {
   logging: false, // set to console.log to see the raw SQL queries
   dialect: 'postgres',
 });
