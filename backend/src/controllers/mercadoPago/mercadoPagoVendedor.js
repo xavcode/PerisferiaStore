26afc3ba8b82598b43fed52820fbc9ac6b967bff
Alan Almeida <alandavidalmeida@gmail.com>
2023-05-26 12:31:53 -0300

    carrito

--- backend/src/controllers/mercadoPago/mercadoPagoVendedor.js
+++ backend/src/controllers/mercadoPago/mercadoPagoVendedor.js
@@ -2,32 +2,33 @@ const mercadopago = require('mercadopago');
 const { Order } = require('../../db');
 
 mercadopago.configure({
-  access_token: process.env.PROD_ACCESS_TOKEN,
+    access_token: process.env.PROD_ACCESS_TOKEN,
 });
 
 const createPreference = async (orderIds) => {
-  const orders = await Promise.all(orderIds.map((orderId) => Order.findByOrderId(orderId)));
+    const orders = await Promise.all(orderIds.map((orderId) => Order.findByOrderId(orderId)));
 
-  const items = orders.map((order) => ({
+    const items = orders.map((order) => ({
     title: order.name,
-    unit_price: order.price,
+    unit_price: order.totalPrice,
     quantity: order.quantity,
-  }));
+    img: order.img,
+    }));
 
-  const preference = {
+    const preference = {
     items: items,
     back_urls: {
-      success: 'https:///store',
-      failure: 'https://www.tu-tienda.com/error',
-      pending: 'https://www.tu-tienda.com/pending',
+        success: 'http://localhost:5173/store',
+        failure: 'http://localhost:5173/error',
+        pending: 'https://localhost:5173/pending',
     },
     auto_return: 'approved',
-  };
+    };
 
-  const response = await mercadopago.preferences.create(preference);
-  return response.body.id;
+    const response = await mercadopago.preferences.create(preference);
+    return response.body.id;
 };
 
 module.exports = {
-  createPreference,
+    createPreference,
 };
