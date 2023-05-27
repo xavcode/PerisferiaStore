c440e751c78da2463c3b976cbfa6ae7dba87e8b6
gabriellondero40@gmail.com <gabriellondero40@gmail.com>
2023-05-26 21:41:45 -0300

    nueva funcion y relacion creadas para el modelo Review

--- backend/src/controllers/Users/createUser.js
+++ backend/src/controllers/Users/createUser.js
@@ -11,23 +11,23 @@ const create_user =  (newUser) => {
 
 const create_record_user = async (req, res) => {
     try {
-        const {id, name, last_name, username, address, password,
-            mail, phone, is_active } = req.body;
-        const fileData = await fs.promises.readFile(req.file.path.toString());
-        const { data, error } = await supabase.storage.from('Usuarios').upload(`${req.file.originalname}`, fileData);
+        const { name, last_name, username, address, password,
+            mail, img, phone, is_active } = req.body;
+    //     const fileData = await fs.promises.readFile(req.file.path.toString());
+    //     const { data, error } = await supabase.storage.from('Usuarios').upload(`${req.file.originalname}`, fileData);
       
-      // Verifica si hubo un error al guardar el archivo
-      if (error) {
-          throw new Error(`Error al guardar el archivo en Supabase: ${error.message}`);
-    }
-    await fs.promises.unlink(req.file.path.toString()); //eliminamos el archivo del sistema de archivos
+    //   // Verifica si hubo un error al guardar el archivo
+    //   if (error) {
+    //       throw new Error(`Error al guardar el archivo en Supabase: ${error.message}`);
+    // }
+    // await fs.promises.unlink(req.file.path.toString()); //eliminamos el archivo del sistema de archivos
     
-    let imageUrl = await supabase.storage.from('Usuarios').getPublicUrl(`${req.file.originalname}`);
+    // let imageUrl = await supabase.storage.from('Usuarios').getPublicUrl(`${req.file.originalname}`);
     
-    console.log(imageUrl.data.publicUrl)
-    let imagenDB = imageUrl.data.publicUrl.toString();
-        const userCreate = await create_user({ id, name, last_name, username, address, password,
-            mail, img: imagenDB, phone, is_active });
+    // console.log(imageUrl.data.publicUrl)
+    // let imagenDB = imageUrl.data.publicUrl.toString();
+        const userCreate = await create_user({ name, last_name, username, address, password,
+            mail, img, phone, is_active });
         return res.status(200).send('<p>Usuario creado con exito</p>')
     } catch (error) {
         return res.status(404).send({ error: error.message });
---
+++ backend/src/models/Review.js
@@ -0,0 +1,20 @@
+const { DataTypes, UUIDV4 } = require('sequelize');
+
+module.exports = (sequelize) => {
+    sequelize.define('Review', {
+        id: {
+            type: DataTypes.STRING,
+            primaryKey: true,
+            defaultValue: UUIDV4
+        },
+        userId: {
+            type: DataTypes.STRING,
+        },
+        comment: {
+            type: DataTypes.STRING
+        }
+    },
+    {
+      timestamps: false
+  })
+}
